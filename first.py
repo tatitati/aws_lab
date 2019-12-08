f535969135c22c628448aa4edcf06da27a2b229b
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-12-08 19:20:19 +0000

    initial commit

---
+++ first.py
@@ -0,0 +1,9 @@
+from troposphere import Ref, Template
+import troposphere.ec2 as ec2
+
+t = Template()
+instance = ec2.Instance("myinstance")
+instance.ImageId = "ami-951945d0"
+instance.InstanceType = "t1.micro"
+t.add_resource(instance)
+print(t.to_json())
